51b26a387f004e8b53795dbc2096c1e8c8c9e371
Vian540 <frafidhermansyah@gmail.com>
2026-03-31 21:23:23 +0700

    First Commit

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,1 +1,20 @@
-fungsi apa?
+void kalkulatorSisaUang() {
+  int uangSaku;
+  int pengeluaran;
+  int sisaUang;
+
+  Serial.println("=== Kalkulator Sisa Uang Jajan ===");
+
+  Serial.print("Masukkan uang saku hari ini: ");
+  while (Serial.available() == 0) {}
+  uangSaku = Serial.parseInt();
+
+  Serial.print("Masukkan estimasi pengeluaran hari ini: ");
+  while (Serial.available() == 0) {}
+  pengeluaran = Serial.parseInt();
+
+  sisaUang = uangSaku - pengeluaran;
+
+  Serial.print("Sisa uang jajan hari ini: ");
+  Serial.println(sisaUang);
+}
